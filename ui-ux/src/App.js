6454494b565d7f7494e720fcafa3b9cd79f7edf6
BertrandGlaser <bertrandglaser@gmail.com>
2018-09-24 11:48:47 +0200

    bolos

--- ui-ux/src/App.js
+++ ui-ux/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Clock from './Clock/Clock.js'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Clock date={new Date()}/>
       </div>
     );
