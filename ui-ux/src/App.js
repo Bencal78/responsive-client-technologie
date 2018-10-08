f6a368a483de69038b0f43a6dd32ab689b0dad77
BertrandGlaser <bertrandglaser@gmail.com>
2018-10-08 12:39:20 +0200

    initialisation du projet

--- ui-ux/src/App.js
+++ ui-ux/src/App.js
@@ -1,78 +1,17 @@
 import React from 'react';
-import Clock from './Clock/Clock.js'
-import Chat from './Chat/Chat.js'
+import Header from './layouts/header'
+import Navigation from './layouts/navigation'
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = { listClock: [], chat: [] };
-    this.start = this.start.bind(this);
-    this.stop = this.stop.bind(this);
-    this.add = this.add.bind(this);
-    this.remove = this.remove.bind(this);
-    this.handleSend = this.handleSend.bind(this);
-  }
-
-  //All methods related to Clock
-  start(index){
-    let clocks = this.state.listClock.slice();
-    clocks[index].run = true;
-    this.setState({ listClock: clocks});
-  }
-
-  stop(index){
-    let clocks = this.state.listClock.slice();
-    clocks[index].run = false;
-    this.setState({ listClock: clocks});
-  }
-
-  add() {
-    this.setState({
-      listClock: this.state.listClock.concat({ date: new Date(), run: true })
-    });
-  }
-
-  remove() {
-    this.setState({
-      listClock: this.state.listClock.slice(0, -1)
-    });
-  }
-
-  displayClocks() {
-    let listItem = this.state.listClock.map((clock, index) =>
-        <li key={index}>
-          <Clock date={clock.date} run={clock.run}/>
-          <button onClick={() => {this.start(index)}}>Start</button>
-          <button onClick={() => {this.stop(index)}}>Stop</button>
-        </li>
-    );
-
-    return (<ul>{listItem}</ul>);
-  }
-
-  //All methods related to Chat
-  handleSend(name, text) {
-    this.setState({
-      chat: this.state.chat.concat(`${name}: ${text}`)
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="Clock">
-          {this.displayClocks()}
-
-          <button onClick={this.add}>New</button>
-          <button onClick={this.remove}>Delete</button>
-        </div>
-        <div className="Chat">
-          <Chat onSend={this.handleSend} name="Chat1" display={this.state.chat}/>
-          <Chat onSend={this.handleSend} name="Chat2" display={this.state.chat}/>
-        </div>
-      </div>
-    );
-  }
+class App extends React.Component{
+    render = () => {
+        return(
+            <div>
+                <Header />
+                <Navigation />
+            </div>
+        )
+    }
 }
+
 
 export default App;
---
+++ ui-ux/src/layouts/header/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import { changeIsMenuOpen } from '../../actions';
+import { withRouter } from 'react-router-dom';
+
+const styles = {
+    root: {
+        width: '100%',
+        backgroundColor:'#fafafa',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        color: '#222'
+    },
+
+    button: {
+        color: '#222',
+        marginRight:'5px',
+        flex: 1
+    },
+};
+
+class Header extends React.Component {
+
+
+    handleToogleIsOpen = () => {
+        const { toogleIsOpen } = this.props;
+        toogleIsOpen();
+    };
+
+    render () {
+        const { classes } = this.props;
+        return(
+            <AppBar
+                position='fixed'
+                className={classes.root}
+            >
+                <Toolbar variant="dense">
+                    <IconButton className={classes.button} onClick={this.handleToogleIsOpen}>
+                        <MenuIcon/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+    toogleIsOpen: PropTypes.func.isRequired,
+};
+
+export default connect(
+    state => ({
+        isOpen: state.isMenuOpen,
+    }),
+    dispatch =>({
+        toogleIsOpen: () => dispatch(changeIsMenuOpen())
+    })
+)(withStyles(styles)(withRouter(Header)));
