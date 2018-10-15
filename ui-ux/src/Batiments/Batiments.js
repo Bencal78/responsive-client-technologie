33d5e5f1cef5fc70080b4c2c61af67581279b633
Bencal78 <benji_96@hotmail.fr>
2018-10-15 09:26:57 +0200

    commit before pull

---
+++ ui-ux/src/Batiments/Batiments.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Batiments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       id = '',
+       name = '',
+       date = null
+     }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+export default Batiments;
---
+++ ui-ux/src/Stat/Stat.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Stat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       id = '',
+       date = null,
+       visitor_number = 0,
+       recipe = 0
+     }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+export default Stat;
